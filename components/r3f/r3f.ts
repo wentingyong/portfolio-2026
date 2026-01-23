c05b3686061c2af5cadcb5eefca3231a2b5b8a6b
wenting yong <ywtzoe@gmail.com>
2026-01-22 23:42:34 -0500

    chore: upgrade r3f deps and postprocessing compat

--- components/r3f/r3f.ts
+++ components/r3f/r3f.ts
@@ -1,3 +1,4 @@
 import './reactCompat'
 
-export { Canvas, useThree } from '@react-three/fiber'
+// Re-export from fiber so reactCompat always runs first.
+export { Canvas, useFrame, useThree } from '@react-three/fiber'
